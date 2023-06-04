552cb8df24c9daaf554f2580abd0df3646e227fe
root <Francoherrera123>
2023-06-04 15:32:14 -0700

    Tasks

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if argc == 1:
         print("0 arguments.")
     else:
-        print(f"{argc - 1}" + "argument" + ("s:" if argc > 2 else ":"))
+        print(f"{argc - 1}" + " argument" + ("s:" if argc > 2 else ":"))
         for idx, arg in enumerate(sys.argv):
             if idx > 0:
                 print("{}: {}".format(idx, arg))
