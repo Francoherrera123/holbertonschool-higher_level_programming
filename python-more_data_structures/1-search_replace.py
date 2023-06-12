8a8b3df7501ffc18e840c89d733be6c98daadc7e
root <Francoherrera123>
2023-06-11 17:49:32 -0700

    Task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-
-#elem = element in my_list
     if (my_list):
         new_list = [replace if elem == search else elem for elem in my_list]
         return new_list
