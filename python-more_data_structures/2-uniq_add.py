ba5b69764ef87e2dac97dc406ccc99f855070323
root <Francoherrera123>
2023-06-11 17:59:01 -0700

    Update

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if (my_list):
-        return sum(set(my_list))
+    if my_list is None:
+        return
+
+    return sum(set(my_list))
--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if (set_1) and (set_2):
-        return (set_1 & set_2)
+    if set_1 is None or set_2 is None:
+        return
+
+    return (set_1 & set_2)
