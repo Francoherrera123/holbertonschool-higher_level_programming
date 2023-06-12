bc3df880e024ed8f21fb846d1354b3f4637206f7
root <Francoherrera123>
2023-06-11 17:29:45 -0700

    Task 9

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    if (a_dictionary):
+        dict_copy = a_dictionary.copy()
+        dict_copy.update((key, value*2) for key, value in a_dictionary.items)
+        return dict_copy
