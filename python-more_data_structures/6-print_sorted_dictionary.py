36f20d82dd6b2b992213c1c75d999f3bfc92dc79
Franco Herrera <80485846+Francoherrera123@users.noreply.github.com>
2023-06-11 22:04:54 -0300

    Fix

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,5 +4,5 @@ def print_sorted_dictionary(a_dictionary):
         return
 
     new_set = {}
-    for key, value in sorted(a_dictionary.items(), key+lambda x: x[0]):
+    for key, value in sorted(a_dictionary.items(), key=lambda x: x[0]):
         print("{}: {}".format(key, value))
