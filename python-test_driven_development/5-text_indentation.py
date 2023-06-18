c18e05344511ebccbc8af75f9659bcfc0fcb310a
root <Francoherrera123>
2023-06-18 16:29:16 -0700

    task 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" module """
+
+
+def text_indentation(text):
+    """ text_identation """
+    space = 0
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    for idx, char in enumerate(text):
+        if char == "." or char == "?" or char == ":":
+            space = 1
+            print(f"{char}")
+            print()
+        if space == 1:
+            if text[idx + 1] == " ":
+                continue
+            else:
+                space = 0
+        else:
+            print(f"{char}", end="")
