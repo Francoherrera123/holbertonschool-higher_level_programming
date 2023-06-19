30cbab80929101bb2e05dccae11da29e418a30f8
root <Francoherrera123>
2023-06-18 18:29:41 -0700

    Tasks

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+
+
+class Rectangle:
+    """Class thast defines a rectangle with attributes"""
+    def __init__(self, widht=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """calculates the area of a rectangle"""
+        return self.__height * self.__width
+
+    def perimeter(self)
+    """calculates the perimeter of a rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            result = (self.__width * 2) + (self.__height * 2)
+            return result
+
