d3da9c18ff9160b8d3edc2fc9b5ce010f7070e82
root <Francoherrera123>
2023-06-25 15:26:58 -0700

    Fix

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,11 +4,11 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Class that defiune a new class based on BaseGeometry class"""
+    """Class that define a new class based on BaseGeometry class"""
 
     def __init__(self, width, height):
         """To create a new instance"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        self.__width == width
-        self.__height == height
+        self.__width = width
+        self.__height = height
