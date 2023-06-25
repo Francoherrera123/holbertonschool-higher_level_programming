3a92efe756ad235055bec94a561ca0083bad4297
root <Francoherrera123>
2023-06-25 16:04:31 -0700

    Tasks

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,7 +6,7 @@ import os.path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filenamee = "add_item.json"
+filename = "add_item.json"
 args = sys.argv[1:]
 new_list = []
 
