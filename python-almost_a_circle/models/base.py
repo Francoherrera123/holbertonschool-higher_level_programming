2bf8a954d46fb0a4a2da4984ef3344e741c53268
root <Francoherrera123@francoh261@gmail.com>
2023-07-02 02:55:44 +0000

    Task 0 & 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a base to the proyect 'almost_a_circle'"""
+import json
+
+class Base:
+    """Base class with attributes and init constructor"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Constructor that generates a new instance of Base class"""
+        if id:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
